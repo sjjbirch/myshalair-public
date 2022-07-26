898b542c8cd072d2439c76726a65cad43912f3e8
sjjbirch <birch.jj@gmail.com>
2022-07-26 15:56:10 +1000

    added admin boolean to users for us 1.8, sp 1.2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   
   get '/boys', to: 'dogs#boys'
   get '/girls', to: 'dogs#girls'
-  get 'puppies', to: 'dogs#puppies'
+  get '/puppies', to: 'dogs#puppies'
+
+  scope 'auth' do
+    post '/signup', to: 'users#create'
+    post '/signin', to: 'users#sign_in'
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
