8510aeb19465747acd20f7e792b69388c5082bd2
sjjbirch <birch.jj@gmail.com>
2022-08-16 18:05:14 +1000

    added description column type text to table dog to support US1.3 and later shows for dog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_16_050337) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_16_080002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_16_050337) do
     t.boolean "retired", default: false
     t.integer "position", null: false
     t.integer "owner_id", default: 1
+    t.text "description", default: "This dog doesn't yet have a description."
     t.index ["position"], name: "index_dogs_on_position"
   end
 
