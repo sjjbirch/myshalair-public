edc513466128568f7eaec19b21e1c9ca93c6ae3d
sjjbirch <birch.jj@gmail.com>
2022-08-25 18:27:59 +1000

    BS.2.11. Confirmation mailer now works in development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,19 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
+  config.action_mailer.default_url_options = { host: 'myshalair.com' }
+  
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
+    port: 587,
+    user_name: Rails.application.credentials.google.email,
+    password: Rails.application.credentials.google.api_password,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
